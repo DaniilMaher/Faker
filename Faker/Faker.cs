865e04bb7d050218830d9d4a87b9d88d7ed104eb
DanilaMaher <daniilnews@gmail.com>
2018-12-03 12:30:33 +0300

    Faker base functions declared

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using ValuesGenerator;
 
 namespace Faker
 {
     public class Faker : IFaker
     {
+        private Dictionary<Type, IBaseTypeValueGenerator> baseTypesGenerators;
+        private Stack<Type> generatedTypes;
+
         public T Create<T>()
         {
-            throw new NotImplementedException();
+            return (T)Create(typeof(T));
+        }
+
+        private object Create(Type type)
+        {
+            object created = null;
+            if (type.IsValueType)
+            {
+                if (baseTypesGenerators.TryGetValue(type, out IBaseTypeValueGenerator generator))
+                {
+                    created = generator.Generate();
+                } 
+                else
+                {
+                    created = Activator.CreateInstance(type);
+                }
+            }
+            else if (type.IsClass && !type.IsGenericType && !type.IsArray && !type.IsPointer && !type.IsAbstract && !generatedTypes.Contains(type))
+            {
+                created = CreateCustomObject(type);
+                FillFieldsAndProperties(created);
+            }
+            return created;
+        }
+
+        private void FillFieldsAndProperties(object o)
+        {
+
+        }
+
+        private object CreateCustomObject(Type type)
+        {
+            return null;
         }
     }
 }
