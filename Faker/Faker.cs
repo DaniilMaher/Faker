45c363a090670afe6d6e1abce82e937afcff3242
DanilaMaher <daniilnews@gmail.com>
2018-12-03 10:55:35 +0300

    Faker interface and class added

---
+++ Faker/Faker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faker
+{
+    class Faker : IFaker
+    {
+        public T Create<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
